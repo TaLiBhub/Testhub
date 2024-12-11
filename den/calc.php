68f760173694d7df4c10cc3ac699c1feded9c2de
Denys Simanchuk <denys.simanchuk@mpfunds.com>
2024-12-11 19:25:01 +0200

    in

--- den/calc.php
+++ den/calc.php
@@ -22,11 +22,11 @@ function calculate($num1, $num2, $operation) {
 // Основна програма
 echo "Консольний калькулятор\n";
 echo "Введіть перше число: ";
-$num1 = trim(fgets(STDIN)); // Читаємо перше число
+$num1 = trim(readline()); // Читаємо перше число
 echo "Введіть операцію (+, -, *, /): ";
-$operation = trim(fgets(STDIN)); // Читаємо операцію
+$operation = trim(readline()); // Читаємо операцію
 echo "Введіть друге число: ";
-$num2 = trim(fgets(STDIN)); // Читаємо друге число
+$num2 = trim(readline()); // Читаємо друге число
 
 // Виконуємо обчислення
 $result = calculate($num1, $num2, $operation);
